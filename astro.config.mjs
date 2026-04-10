602b03bfe12d1108a395ed120c7d61977796f491
“gyeon” <jkp.112357@gmail.com>
2026-04-10 23:23:01 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false
-  },
+  devToolbar: { enabled: false },
   prefetch: true,
   site: 'https://hausofmatters-main.vercel.app/',
   integrations: [sitemap()],
-  experimental: {
-    svg: true,
-  },
+  experimental: { svg: true },
+  vite: {
+    optimizeDeps: {
+      exclude: ['@astrojs/compiler']
+    }
+  }
 });
