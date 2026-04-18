d126566d126614f8cd9671dcc06b8434dada3eb2
“gyeon” <jkp.112357@gmail.com>
2026-04-18 23:56:47 +0900

    update: interview

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,49 +2,85 @@ import { defineCollection, reference, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const artists = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data/artists" }),
-  schema: z.object({
-    name: z.string(),
-    stage_name: z.string(),
-    genre: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+ loader: glob({ pattern: "**/*.md", base: "./src/data/artists" }),
+ schema: z.object({
+  name: z.string(),
+  stage_name: z.string(),
+  genre: z.string(),
+  image: z.object({
+   src: z.string(),
+   alt: z.string(),
   }),
+ }),
 });
 
 const albums = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data/albums" }),
-  schema: z.object({
-    name: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.date(), // e.g. 2024-09-17
-    tracks: z.array(z.string()),
-    artist: reference("artists"),
+ loader: glob({ pattern: "**/*.md", base: "./src/data/albums" }),
+ schema: z.object({
+  name: z.string(),
+  image: z.object({
+   src: z.string(),
+   alt: z.string(),
   }),
+  publishDate: z.date(), // e.g. 2024-09-17
+  tracks: z.array(z.string()),
+  artist: reference("artists"),
+ }),
 });
 
 const magazine = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data/magazine" }),
-  schema: z.object({
-    title: z.string().optional(),
-    slug: z.string().optional(),
-    pdfPath: z.string(),
-    date: z.date(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }).optional(),
-  }),
+ loader: glob({ pattern: "**/*.md", base: "./src/data/magazine" }),
+ schema: z.object({
+  title: z.string().optional(),
+  slug: z.string().optional(),
+  pdfPath: z.string(),
+  date: z.date(),
+  image: z
+   .object({
+    src: z.string(),
+    alt: z.string(),
+   })
+   .optional(),
+ }),
+});
+
+const interview = defineCollection({
+ loader: glob({ pattern: "**/*.md", base: "./src/data/interview" }),
+ schema: z.object({
+  artist: reference("artists"),
+  date: z.date(),
+  published: z.boolean().default(true),
+  coverImage: z
+   .object({
+    src: z.string(),
+    alt: z.string(),
+   })
+   .optional(),
+  text: z.array(
+   z.object({
+    q: z.string(),
+    a: z.array(
+     z.object({
+      text: z.string(),
+      images: z
+       .array(
+        z.object({
+         src: z.string(),
+         alt: z.string(),
+        })
+       )
+       .optional(),
+     })
+    ),
+   })
+  ),
+ }),
 });
 
 // Export all collections
 export const collections = {
-  artists,
-  albums,
-  magazine,
+ artists,
+ albums,
+ magazine,
+ interview,
 };
